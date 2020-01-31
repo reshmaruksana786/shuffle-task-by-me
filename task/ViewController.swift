227ebe1d24e08d4bbe6b138dfe5df151364da9e5
reshma <reshmaruksana786786@gmail.com>
2020-01-31 17:03:27 +0530

    Initial Commit

---
+++ task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  task
+//
+//  Created by Syed.Reshma Ruksana on 1/31/20.
+//  Copyright © 2020 Syed.Reshma Ruksana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
